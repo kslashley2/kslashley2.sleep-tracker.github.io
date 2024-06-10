8579cbb157c1308697359583246647bafab576c8
kslashley2 <142209634+kslashley2@users.noreply.github.com>
2024-06-10 20:19:52 +1000

    Establishing root folders of file

---
+++ server.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+ })
+
+ app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
